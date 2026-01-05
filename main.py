f4992556d836f30f0a217356460bc23ff40bb362
Polybunny <andrewberlinghoff@gmail.com>
2026-01-05 13:01:27 -0500

    added player.py and circleshape.py, and modified gameplay loop to draw player triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,7 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     # Make game loop
     while True:
         log_state()
@@ -19,9 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        
+
+
 
 
 if __name__ == "__main__":
